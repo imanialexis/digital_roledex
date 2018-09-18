017abf4eba7c1f6b84a15fb7441402a4a689b79f
imani alexis <alexis.bailey37@gmail.com>
2018-09-18 16:16:21 -0400

    added associations in models

--- app/models/affair_contact.rb
+++ app/models/affair_contact.rb
@@ -1,2 +1,4 @@
 class AffairContact < ApplicationRecord
+    belongs_to :affair 
+    belongs_to :contact
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+    belongs_to :user
+    has_many :affair_contacts
+    has_many :affairs, through: :affair_contacts
+
+
 end
+
+
+
+
+
+
