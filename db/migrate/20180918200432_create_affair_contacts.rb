cecbd9bae2c41859695bcc4e4e5a0efea8c5cd60
imani alexis <alexis.bailey37@gmail.com>
2018-09-22 19:42:13 -0400

    if current user has no avatat, default image selected

--- db/migrate/20180918200432_create_affair_contacts.rb
+++ db/migrate/20180918200432_create_affair_contacts.rb
@@ -2,7 +2,7 @@ class CreateAffairContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :affair_contacts do |t|
       t.integer :contact_id
-      t.integer :event_id
+      t.integer :affair_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Affair.create(
+#    name:'LWT Summit',
+#    location: 'New York',
+#     website: 'https://lesbianswhotech.org/',
+#     date: Time.now,
+#     description: 'Disrupt effective white paper relief efficient impact. Save the world shared vocabulary, living a fully ethical life changemaker collective impact accessibility co-create correlation. Policymaker, engaging B-corp living a fully ethical life grit systems thinking. Circular challenges and opportunities, because effective incubator. Relief boots on the ground venture philanthropy radical think tank. Revolutionary shared vocabulary corporate social responsibility ecosystem families. '
+# )
+
+
+# Affair.create(
+#     name:'TechActivist',
+#     location: 'New York',
+#     website: 'TechActivist.org/',
+#     date: Time.now,
+#     description: 'Youth challenges and opportunities, program area, greenwashing, policymaker improve the world social impact thought provoking. Milestones problem-solvers accessibility save the world justice, theory of change cultivate scalable. Collaborative consumption, human-centered overcome injustice when, compelling empathetic thought partnership resilient. Social enterprise collaborative consumption communities society youth '
+
+# )
+
+# Affair.create(
+#     name:'Empire.JS',
+#     location: 'New York',
+#     website: 'empirejs.org/',
+#     date: Time.now,
+#     description: 'Circular challenges and opportunities, because effective incubator. Relief boots on the ground venture philanthropy radical think tank. Revolutionary shared vocabulary corporate social responsibility ecosystem families. Youth challenges and opportunities, program area, greenwashing, policymaker improve the world social impact thought provoking.'
+
+# )
+
+# Affair.create(
+#     name:'Free Code Camp Meetup',
+#     location: 'New York',
+#     website: 'freecodecamp.org/',
+#     date: Time.now,
+#     description: 'Milestones or; blended value green space co-creation design thinking thought partnership B-corp. Accessibility social return on investment; sustainable fairness best practices natural resources. Agile; design thinking rubric; problem-solvers thought provoking co-create. Data thought partnership game-changer grit empower move the needle empower. Impact then, incubator collaborate resist problem-solvers. ',
+        # user_id: 1
+# )
+
+# Affair.create(
+#     name:'Code & Mix',
+#     location: 'New York',
+#     website: 'webuildblack.com',
+#     date: Time.now,
+#     description: 'The ideate, corporate social responsibility mass incarceration sustainable collaborate social impact social return on investment. B-corp, social innovation human-centered activate, then innovate. Deep dive activate inclusion save the world transparent collaborative cities empower communities her body her rights. Blended value ideate synergy; then shine triple bottom line. Correlation strengthening infrastructure, framework, communities program areas activate external partners '
+# )
