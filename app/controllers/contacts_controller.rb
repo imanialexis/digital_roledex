13019057955fc40d35c1a44c934d995ff09c6ba9
imani alexis <alexis.bailey37@gmail.com>
2018-09-24 16:16:50 -0400

    affair_contacts working now

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -25,21 +25,29 @@ class ContactsController < ApplicationController
   # POST /contacts.json
   def create
    
-   
-    @contact = Contact.new(contact_params)
-    @contact_event = Affair_Contacts.new(contact_id: @contact.id, affair_id: params[:affair_id])
-    @contact_event.save
+   puts ' IM IN THE CREATE METHOD '
+    contact = Contact.new(contact_params)
+    contact.save
+    puts "THIS IS THE CONTACT ID"
+    puts contact.id
+    puts "THIS IS THE CONTACT ID"
+    puts "THIS IS THE AFFAIR ID"
+    puts params[:affair][:affair_id]
+    puts "THIS IS THE AFFAIR ID"
+    contact_event = AffairContact.new(contact_id: contact.id, affair_id: params[:affair][:affair_id])
+    contact_event.save
 
     
     respond_to do |format|
-      if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render :show, status: :created, location: @contact }
+      if contact.save
+        format.html { redirect_to contact, notice: 'Contact was successfully created.' }
+        format.json { render :show, status: :created, location: contact }
       else
         format.html { render :new }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
+        format.json { render json: contact.errors, status: :unprocessable_entity }
       end
     end
+
   end
 
   # PATCH/PUT /contacts/1
@@ -74,7 +82,7 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:fname, :lname, :email, :title, :phone, :website, :linked_in, :facebook, :twitter, :other_social, :highlights, :notes, :avatar, :user_id)
+      params.require(:contact).permit(:fname, :lname, :email, :title, :pro_designation, :company, :location ,:phone, :website, :linked_in, :facebook, :twitter, :other_social, :highlights, :notes, :avatar, :user_id)
     end
 
 end
