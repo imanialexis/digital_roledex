d06271271b56046703fc39867f8b1ac6d31e772c
imani alexis <alexis.bailey37@gmail.com>
2018-09-26 13:35:00 -0400

    added linkedin gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:linkedin]
+
+      def self.from_omniauth(access_token)
+          data = access_token.info
+          puts '========='
+          puts data
+          puts '========='
+
+          user = User.where(email: data['email']).first
+      
+          # Uncomment the section below if you want users to be created if they don't exist
+          if !user
+              user = User.create(fname: data['first_name'],
+                lname: data['last_name'],
+                 email: data['email'], 
+                 location: data["location"]["name"],
+                 password: Devise.friendly_token[0,20]
+              )
+          end
+          
+          user
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homepage#landing_page'
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' , omniauth_callbacks: 'omniauth_callbacks'}
+
   resources :affairs
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
