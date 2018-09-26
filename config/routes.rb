8ad35ad42f0a2c30441d216360e5cd9ef36bd603
imani alexis <alexis.bailey37@gmail.com>
2018-09-26 15:01:24 -0400

    added route and page for how it works

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/profile' => 'homepage#user_homepage'
+  get "/howitworks" => 'homepage#how_it_works'
 
 
 end
